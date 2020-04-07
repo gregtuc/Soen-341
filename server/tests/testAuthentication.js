af74aa5091173de878313723c2a597430950e989
Greg <60011793+gregtuc@users.noreply.github.com>
2020-04-07 11:48:42 -0400

    new test

--- server/tests/testAuthentication.js
+++ server/tests/testAuthentication.js
@@ -5,48 +5,54 @@ var mongoose = require("mongoose");
 //Data being sent to login route.
 const userCredentials = {
   email: "Greg.tucker1@hotmail.ca",
-  password: "cookie123"
+  password: "cookie123",
 };
 
-describe("GET /api/currentuser", function() {
+describe("My API Tests", function () {
   //POST the userCredentials to /api/login, and expect a response of status 200.
   var server = require("../index");
-  var authenticatedUser = request.agent(server);
+  var app = request.agent(server);
+  var token = null;
 
-  before(function(done) {
-    authenticatedUser
+  //Before running tests, login user and get JWT for testing.
+  before(function (done) {
+    app
       .post("/api/login")
       .send(userCredentials)
-      .end(function(err, response) {
+      .end(function (err, response) {
+        token = res.data.token;
         expect(response.statusCode).to.equal(200);
         done();
       });
   });
-  //Status code 200 should have been sent.
 
-  it("should return a 200 response if the user can log in", function(done) {
-    authenticatedUser.get("http://localhost:8000/api/login", function(
-      err,
-      response,
-      body
-    ) {
+  it("Should return a list of followers"),
+    function (done) {
+      request
+        .get("http://localhost:8000/api/follow")
+        .set({ token: sessionStorage.getItem("jwt") })
+        .end((err, res) => {
+          res.statusCode.should.equal(200);
+          res.data.following.should.be.instanceOf(Array);
+        });
+      done();
+    };
+
+  it("should return a 200 response if the user can log in", function (done) {
+    app.get("http://localhost:8000/api/login", function (err, response, body) {
       response.statusCode.should.equal(200);
       body.should.include("Token");
     });
     done();
   });
-  it("should return a 401 response if the credentials are invalid", function(done) {
-    authenticatedUser.get("http://localhost:8000/api/login", function(
-      err,
-      response,
-      body
-    ) {
+  it("should return a 401 response if the credentials are invalid", function (done) {
+    app.get("http://localhost:8000/api/login", function (err, response, body) {
       response.statusCode.should.equal(401);
     });
     done();
   });
-  after(function(done) {
-    server.close(function() {
+  after(function (done) {
+    server.close(function () {
       mongoose.connection.close(done);
     });
     process.exit(0);
